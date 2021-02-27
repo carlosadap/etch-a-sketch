94db9d9c9d8a0a3a1c09392ab0aa426e0b5066b3
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-27 10:20:53 +0100

    Add change color on mouseover

--- app.js
+++ app.js
@@ -3,9 +3,34 @@ const output = document.querySelector('.size-output');
 const wrapper = document.querySelector('.wrapper');
 
 output.textContent = size.value;
+createGrid(size.value);
 
 size.addEventListener('input', function () {
   output.textContent = size.value;
   wrapper.style.gridTemplateRows = `repeat(${size.value}, 1fr)`;
   wrapper.style.gridTemplateColumns = `repeat(${size.value}, 1fr)`;
+  createGrid(size.value);
 });
+
+function createGrid(n) {
+  let idx = 0;
+  let wrapper = document.querySelector('.wrapper');
+  wrapper.innerHTML = "";
+  while (idx < n * n) {
+    const div = document.createElement('div');
+    div.className = `cell cell-${idx}`;
+    wrapper.appendChild(div);
+    idx++;
+  }
+  addCellListener();
+}
+
+function addCellListener() {
+  const cells = document.querySelectorAll('.cell')
+  cells.forEach(cell => cell.addEventListener('mouseover', changeColor))
+}
+
+function changeColor() {
+  const color = "black";
+  this.style.backgroundColor = color;
+}
